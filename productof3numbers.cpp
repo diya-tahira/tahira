586be15993b243c7f56d7929a708f068beecaa58
diya-tahira <120877885+diya-tahira@users.noreply.github.com>
2023-03-25 15:28:18 +0500

    Add files via upload

---
+++ productof3numbers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float a,b,c,prod;
+    cout<<"Enter first number= ";
+    cin>>a;
+    cout<<"\nEnter second number= ";
+    cin>>b;
+    cout<<"\nEnter third number= ";
+    cin>>c;
+    prod=a*b*c;
+    cout<<"Product= "<<prod;
+    
+}
